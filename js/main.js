bb16d0399c2e57c166722c4cb7fda40527825af8
José Israel Santizo Santos <joseisraelss.JISS@gmail.com>
2021-06-20 19:21:21 -0600

    Se agrega funcion a cada select para modificar el tamanio del la barra skill

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 //Tomamos en una costante la sentencia document para usarla sin tener que escribir la sentencia completa de nuevo
 const style = document.documentElement.style;
+
+/* Cambia el width al presionar un boton
 const tamanio = () => {
     style.setProperty('--width', '80%');
+}
+*/
+const handChangeWidth1 = () => {
+    value = document.getElementById('selectWidth1').value;
+    style.setProperty('--width1', value);
+    console.log(value);
+}
+const handChangeWidth2 = () => {
+    value = document.getElementById('selectWidth2').value;
+    style.setProperty('--width2', value);
+    console.log(value);
+}
+const handChangeWidth3 = () => {
+    value = document.getElementById('selectWidth3').value;
+    style.setProperty('--width3', value);
+    console.log(value);
+}
+const handChangeWidth4 = () => {
+    value = document.getElementById('selectWidth4').value;
+    style.setProperty('--width4', value);
+    console.log(value);
+}
+const handChangeWidth5 = () => {
+    value = document.getElementById('selectWidth5').value;
+    style.setProperty('--width5', value);
+    console.log(value);
 }
